250d16215104054d56b9c9324eb43f4a4780ce90
chatgpt2-crypto <chatgpt2@formetest.abrdns.com>
2026-02-08 01:14:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite');
+// إنشاء ملف قاعدة البيانات
+const db = new sqlite3.Database("./database.sqlite", (err) => {
 
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-  )`);
+  if (err) {
+    console.error("Database error:", err.message);
+  } else {
+    console.log("Connected to SQLite database.");
+  }
+
 });
 
 module.exports = db;
